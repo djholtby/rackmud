89a25e11338d62fd1e5f05d6db461df9bdaab204
Dan Holtby <djholtby@gmail.com>
2017-03-20 17:31:39 -0400

    oops, forgot the modified files

--- src/racket/griftos/main.rkt
+++ src/racket/griftos/main.rkt
@@ -1,18 +1,22 @@
-(module griftos racket/base
-  (require racket/base
-           racket/class
-           racket/undefined
-           "racket-mud.rkt")
-  (#%provide
-   (all-from-except racket/base
+#lang racket/base
+
+(require racket/base
+         racket/class
+         racket/undefined
+         "racket-mud.rkt")
+
+(provide
+   (except-out (all-from-out racket/base)
                     struct
                     define-struct ;; probably need more to cut
                     )
-   (all-from-except racket/class) ;; I'm sure there's stuff that should be cut...
-   (all-from racket/undefined)
-   (all-from-except "racket-mud.rkt"
-                    define-mud-struct
-                    )
-   (rename  define-mud-struct struct)
-   ))
+   (except-out (all-from-out racket/class)) ;; I'm sure there's stuff that should be cut...
+   (all-from-out racket/undefined)
 
+   (except-out (all-from-out "racket-mud.rkt") 
+               define-mud-struct)
+   (rename-out  [define-mud-struct struct]))
+
+
+(module reader syntax/module-reader
+  griftos)
