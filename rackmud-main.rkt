1f198f9b02506bc6418fa91bca985a45c3b74d35
Dan Holtby <djholtby@gmail.com>
2019-10-26 19:07:13 -0400

    move config stuff from rackmud package to here

--- rackmud-main.rkt
+++ rackmud-main.rkt
@@ -1,8 +1,62 @@
 #lang rackmud
 
 (define t0 (current-inexact-milliseconds))
-(require net/rfc6455 racket/tcp openssl rackmud/rackmud-config rackmud/telnet rackmud/websock rackmud/charset "defaults.rkt")
+(require net/rfc6455 racket/tcp openssl rackmud/telnet rackmud/websock rackmud/charset "defaults.rkt")
 (define-namespace-anchor anc)
+
+
+(require racket/hash racket/cmdline)
+(provide load-rackmud-settings)
+
+(define default-config
+  '#hasheq((mudlib-path . "./mudlibs/mudlib")
+           (mudlib-collect . "mudlib")
+           (master-module . "main.rkt")
+           (master-classname . "custom-master%")
+           (thread-count . 4)
+           (telnet:port . 4000)
+           (telnet:encodings . ("ASCII"))
+           (webserver:port . 8000)
+           (webserver:www-path . "/www")
+           (webserver:servlet-url . "/servlet")
+           (webserver:websock-url . "/socket")))
+
+
+(define (config-merge base adjustments)
+  (hash-union base adjustments
+              #:combine
+              (lambda (bv av)
+                av)))
+
+
+
+(define (load-rackmud-settings)
+  (define config-name "rackmud-config.rktd")
+  (define overrides (make-hasheq))
+  (command-line
+   #:once-each
+
+   [("-c" "--config") cfg "loads settings from <cfg> (subsequent flags will override)" (set! config-name cfg)]
+   [("-p" "--telnet-port") port "runs telnet on <port>"
+    (hash-set! overrides 'telnet:port (string->number port))]
+   [("-s" "--secure-telnet-port") port "runs secure telnet on <port> (implies -S)"
+    (hash-set! overrides 'telnet:ssl-port (string->number port))]
+   [("-C" "--ssl-cert") cert "path to SSL sertificate"
+    (hash-set! overrides 'ssl:certificate cert)]
+   [("-K" "--ssl-key") key "path to SSL private key"
+    (hash-set! overrides 'ssl:private-key key)]
+    
+   #:once-any
+   [("-i" "--interactive") "run with a GriftOS REPL hooked into STDIN and STDOUT" (hash-set! overrides 'interactive #t)]
+   [("--not-interactive") "disables REPL (if enable by cfg file" (hash-set! overrides 'interactive #f)]
+   
+   #:once-any
+   [("-S" "--secure-telnet")  "enables secure telnet" (hash-set! overrides 'telnet:ssl #t)]
+   [("-N" "--no-secure-telnet") "disables secure telnet" (hash-set! overrides 'telnet:ssl #f)])
+  
+  (define loaded-settings (with-input-from-file config-name read))
+  (config-merge (config-merge default-config loaded-settings) overrides))
+
 (define cfg (load-rackmud-settings))
 
 (define telnet-port (hash-ref cfg 'telnet:port #f))
@@ -35,7 +89,7 @@
   (encodings->charset-req-sequence telnet-encodings))
 
 (unless (or telnet-enabled? telnet-ssl-enabled? http-enabled? https-enabled?)
-  (error 'rackmud "telnet and websock are both disabled -- rackmudos must have at least some kind of outside connection!"))
+  (error 'rackmud "telnet and websock are both disabled -- rackmud must have at least some kind of outside connection!"))
 
 (display "Establishing Database Connection...")
 (set! t0 (current-inexact-milliseconds))
@@ -71,10 +125,10 @@
 ;;(when mudlib (set-lib-path! mudlib))
 
 
-(parameterize ([current-library-collection-links (if mudlib/path
-                                                     (cons (hasheq mudlib-collect (list mudlib/path)) (current-library-collection-links))
-                                                     (current-library-collection-links))])
-
+(parameterize ([current-library-collection-links
+                (if mudlib/path
+                    (cons (hasheq mudlib-collect (list mudlib/path)) (current-library-collection-links))
+                    (current-library-collection-links))])
   
   (define resolved-collect (collection-file-path mudlib-module (symbol->string mudlib-collect) #:fail
                                                  (lambda (message)
