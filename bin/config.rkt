ed288eaaa7146ca4e4d3a488e466b488a8dc8ffc
Dan Holtby <djholtby@gmail.com>
2021-11-08 12:12:55 -0500

    work around for 8.3 regression on hash-union

--- bin/config.rkt
+++ bin/config.rkt
@@ -6,61 +6,70 @@
 (define default-config-name "rackmud-config.rktd")
 
 (define default-config
-  `#hasheq((mudlib-path . #f)
-           (mudlib-collect . "mudlib")
-           (mudlib-runtime-path . "./runtime-collects/")
-           (master-module . "main.rkt")
-           (master-classname . "custom-master%")
-           (thread-count . 16)
-           (telnet:port . 4000)
-           (telnet:encodings . ("ASCII"))
-           (webserver:port . 8000)
-           (webserver:www-path . "/www")
-           (webserver:servlet-url . "/servlet")
-           (webserver:websock-url . "/socket")
-           (webserver:server-directory . "/")
-           (webserver:proxy-mode . #f)
-           (filename . ,default-config-name)))
+  (make-immutable-hash
+   `((mudlib-path . #f)
+     (mudlib-collect . "mudlib")
+     (mudlib-runtime-path . "./runtime-collects/")
+     (master-module . "main.rkt")
+     (master-classname . "custom-master%")
+     (thread-count . 16)
+     (telnet:port . 4000)
+     (telnet:encodings . ("ASCII"))
+     (webserver:port . 8000)
+     (webserver:www-path . "/www")
+     (webserver:servlet-url . "/servlet")
+     (webserver:websock-url . "/socket")
+     (webserver:server-directory . "/")
+     (webserver:proxy-mode . #f)
+     (filename . ,default-config-name))))
 
      
 (define (config-merge base adjustments)
   (hash-union base adjustments
               #:combine
-              (lambda (bv av)
-                av)))
+              (lambda (base-val adjusted-val)
+                adjusted-val)))
 
 
 
 (define (load-rackmud-settings)
   (define config-name default-config-name)
-  (define overrides (make-hasheq))
+  (define overrides (box (hasheq)))
+  (define (override-set! key val)
+    (set-box! (hash-set (unbox overrides) key val)))
+  
   (command-line
    #:once-each
 
    [("-c" "--config") cfg "loads settings from <cfg> (subsequent flags will override)"
-    (set! config-name cfg)
-    (hash-set! overrides 'filename cfg)]
-   [("-b" "--build") "compiles the mudlib (and then terminates)" (hash-set! overrides 'build-only #t)]
-   [("-B" "--skip-build") "skips the initial compile of mublib " (hash-set! overrides 'compile-mudlib-on-launch? #f)]
+                      (set! config-name cfg)
+                      ( 'filename cfg)]
+   [("-b" "--build") "compiles the mudlib (and then terminates)"
+                     (override-set! 'build-only #t)]
+   [("-B" "--skip-build") "skips the initial compile of mublib "
+                          (override-set! 'compile-mudlib-on-launch? #f)]
    [("-p" "--telnet-port") port "runs telnet on <port>"
-    (hash-set! overrides 'telnet:port (string->number port))]
+                           (override-set! 'telnet:port (string->number port))]
    [("-s" "--secure-telnet-port") port "runs secure telnet on <port> (implies -S)"
-    (hash-set! overrides 'telnet:ssl-port (string->number port))]
+                                  (override-set! 'telnet:ssl-port (string->number port))]
    [("-C" "--ssl-cert") cert "path to SSL sertificate"
-    (hash-set! overrides 'ssl:certificate cert)]
+                        (override-set! 'ssl:certificate cert)]
    [("-K" "--ssl-key") key "path to SSL private key"
-    (hash-set! overrides 'ssl:private-key key)]
+                       (override-set! 'ssl:private-key key)]
    [("-r" "--runtime-path") path "path to be used for the runtime mudlib collection"
-    (hash-set! overrides 'mudlib-runtime-path path)]
+                            (override-set! 'mudlib-runtime-path path)]
    #:once-any
-   [("-i" "--interactive") "run with a Racket REPL hooked into STDIN and STDOUT" (hash-set! overrides 'interactive #t)]
-   [("--not-interactive") "disables REPL (if enable by cfg file)" (hash-set! overrides 'interactive #f)]
+   [("-i" "--interactive") "run with a Racket REPL hooked into STDIN and STDOUT"
+                           (override-set! 'interactive #t)]
+   [("--not-interactive") "disables REPL (if enable by cfg file)"
+                          (override-set! 'interactive #f)]
    
    #:once-any
-   [("-S" "--secure-telnet")  "enables secure telnet" (hash-set! overrides 'telnet:ssl #t)]
-   [("-N" "--no-secure-telnet") "disables secure telnet" (hash-set! overrides 'telnet:ssl #f)])
+   [("-S" "--secure-telnet")  "enables secure telnet" (override-set! 'telnet:ssl #t)]
+   [("-N" "--no-secure-telnet") "disables secure telnet" (override-set! 'telnet:ssl #f)])
   
-  (define loaded-settings (with-handlers ([exn:fail:filesystem? (λ (e) (make-hasheq '((no-file? . #t))))])
-                            (make-hasheq (with-input-from-file config-name read))))
+  (define loaded-settings (with-handlers ([exn:fail:filesystem? (λ (e) (make-immutable-hasheq
+                                                                        '((no-file? . #t))))])
+                            (make-immutable-hasheq (with-input-from-file config-name read))))
   
-  (config-merge (config-merge default-config loaded-settings) overrides))
+  (config-merge (config-merge default-config loaded-settings) (unbox overrides)))
